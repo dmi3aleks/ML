f8259639294ea5debe6cdb80984585df32421e2e
dmitry <dmitry@localhost.localdomain>
2018-02-25 17:26:24 +0900

    ML: Collaborative Filtering: vectorized implementation of the cost function and its gradients

--- mle8/ex8/cofiCostFunc.m
+++ mle8/ex8/cofiCostFunc.m
@@ -11,30 +11,18 @@ X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
-J = 0;
-for i = 1:num_movies
-  for j = 1:num_users
-    if R(i,j) == 1
-      J += (X(i,:) * Theta(j,:)' - Y(i,j)).^2;
-    end
-  end
-end
-J = J/2;
-
+J = sum(sum((R .* (X * Theta' - Y)).^2))/2;
 regularization  = (lambda/2) * (sum(sum(Theta .^2)) + sum(sum(X .^2)));
 J += regularization;
 
 %        X_grad - num_movies x num_features matrix, containing the
 %                 partial derivatives w.r.t. to each element of X
-
 X_grad = zeros(size(X));
 for i = 1:num_movies
-  for j = 1:num_users
-    if R(i,j) == 1
-      X_grad(i,:) += (X(i,:) * Theta(j,:)' - Y(i,j)) * Theta(j,:);
-    end
-  end
-  X_grad(i,:) += lambda * X(i,:);
+  idx = find(R(i,:)==1);
+  Theta_t = Theta(idx,:);
+  Y_t = Y(i,idx);
+  X_grad(i,:) = (X(i,:) * Theta_t' - Y_t) * Theta_t + lambda * X(i,:);
 end
 
 %        Theta_grad - num_users x num_features matrix, containing the
@@ -50,6 +38,18 @@ for j = 1:num_users
   Theta_grad(j,:) += lambda * Theta(j,:);
 end
 
-grad = [X_grad(:); Theta_grad(:)];
+% X     = [num movies, num features]
+% Theta = [num users,  num features]
+% Y     = [num movies, num users]
 
+Theta_grad = zeros(size(Theta));
+for j = 1:num_users
+  idx = find(R(:,j)==1);
+  X_t = X(idx,:);
+  Y_t = Y(idx,j);
+  Theta_grad(j,:) = (X_t * Theta(j,:)' - Y_t)' * X_t;
+  Theta_grad(j,:) += lambda * Theta(j,:);
+end
+
+grad = [X_grad(:); Theta_grad(:)];
 end
